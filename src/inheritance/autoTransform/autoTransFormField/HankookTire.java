8d0f185e2c62ebacaa56e0f3c317df5095805888
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2022-01-11 22:36:14 +0900

    자동타입변환(필드의 다형화)

---
+++ src/inheritance/autoTransform/autoTransFormField/HankookTire.java
@@ -0,0 +1,28 @@
+package inheritance.autoTransform.autoTransFormField;
+
+public class HankookTire extends Tire {
+    //필드
+    //생성자
+    public HankookTire(String location, int maxRotation) {
+        super(location, maxRotation);
+    }
+
+    //메소드
+    @Override
+    public boolean roll() {
+        ++accumulatedRotation; //누적 회전수 1증가
+        if (accumulatedRotation < maxRotation) {
+            System.out.println(location + "HankookTire 수명: " +
+                (maxRotation-accumulatedRotation) + "회");
+            return true;
+        }
+        else {
+            System.out.println("*** " + location + " HankookTire 펑크 ***");
+            return false;
+        }
+    }
+
+    public void Print() {
+        System.out.println("test");
+    }
+}
