9cac85c95d10b98972def6aaaae005697248c60c
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2022-01-26 11:25:59 +0900

    스레드 협업 wait(), notify()

---
+++ src/multiThread/waitAndNotifyAndNotifyAll/dataWriteAndReadEx/WaitNotifyExample.java
@@ -0,0 +1,14 @@
+package multiThread.waitAndNotifyAndNotifyAll.dataWriteAndReadEx;
+
+public class WaitNotifyExample {
+
+    public static void main(String[] args) {
+        DataBox dataBox = new DataBox();
+
+        ProducerThread producerThread = new ProducerThread(dataBox);
+        ConsumerThread consumerThread = new ConsumerThread(dataBox);
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
