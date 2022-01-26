0519ab321c4a886f1692776fbfca394741d2b38d
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2022-01-26 21:28:23 +0900

    스레드 협업 메소드 wait(), notify()

---
+++ src/multiThread/controlThreadState/waitAndNotifyAndNotifyAll/dataWriteAndReadEx/ConsumerThread.java
@@ -0,0 +1,18 @@
+package multiThread.controlThreadState.waitAndNotifyAndNotifyAll.dataWriteAndReadEx;
+
+public class ConsumerThread extends Thread {
+
+    private DataBox dataBox;
+
+    public ConsumerThread(DataBox dataBox) {
+        this.dataBox = dataBox;
+    }
+
+    @Override
+    public void run() {
+       // for (int i = 1; i <= 3; i++) {
+            String data = dataBox.getData();
+        //}
+        System.out.println("ConsumerThread 종료");
+    }
+}
