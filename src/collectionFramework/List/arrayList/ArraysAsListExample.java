765cde39f7c8fdbd6de9c2e61bbcd1d8d88431c2
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2022-02-17 17:59:47 +0900

    ArrayAsList

---
+++ src/collectionFramework/List/arrayList/ArraysAsListExample.java
@@ -0,0 +1,20 @@
+//초기 선언때 객체값을 넣어줄 수 있는 Arrays.asList
+package collectionFramework.List.arrayList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysAsListExample {
+
+    public static void main(String[] args) {
+        List<String> list1 = Arrays.asList("홍길동", "신용권", "감자바");
+        for (String name : list1) {
+            System.out.println(name);
+        }
+
+        List<Integer> list2 = Arrays.asList(1, 2, 3);
+        for (int value : list2) {
+            System.out.println(value);
+        }
+    }
+}
